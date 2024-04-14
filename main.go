e02e36ec9e4bf0084e099548119af654a02acc15
edwinjavier28 <erico.andes@gmail.com>
2024-04-13 19:02:23 -0500

    modified factorial

--- main.go
+++ main.go
@@ -2,15 +2,56 @@ package main
 
 import "fmt"
 
+var b string
+var resultado2 int
+
 func main() {
 	// Número para el cual queremos imprimir la tabla de multiplicar
-	for numero := 1; numero <= 9; numero++ 
+	numero1 := 10
+	result := factorial(numero1)
+	fmt.Println(result)
+	//numero2 := 9
+	//result := multiplicar(numero1, numero2)
+	//fmt.Println(result)
+	//result2 := Isprimo(numero1)
+	//fmt.Println(result2)
 
-	// Imprimimos la tabla de multiplicar del número
-	fmt.Println("Tabla de multiplicar del", numero, ":")
-	for i := 1; i <= 10; i++ {
-		resultado := numero * i
-		fmt.Printf("%d x %d = %d\n", numero, i, resultado)
+}
+
+func multiplicar(numero1, numero2 int) string {
+
+	for numero := numero1; numero <= numero2; numero++ {
+
+		// Imprimimos la tabla de multiplicar del número
+		fmt.Println("Tabla de multiplicar del", numero, ":")
+		for i := 1; i <= 10; i++ {
+			resultado := numero * i
+			a, _ := fmt.Printf("%d x %d = %d\n", numero, i, resultado)
+			b = string(a)
+		}
 	}
 
+	return string(b)
+}
+
+func Isprimo(numero int) string {
+	var resultado string
+
+	if numero/numero == 0 {
+		resultado = "es primo " + string(numero)
+	} else {
+		resultado = "no es primo " + string(numero)
+	}
+	return resultado
+}
+
+// dos funciones que busquen Factorial de un numero
+func factorial(n int) int {
+	// Caso base: si n es 0 o 1, el factorial es 1
+	if n == 0 || n == 1 {
+		return 1
+	}
+
+	// Caso recursivo: n * factorial(n-1)
+	return n * factorial(n-1)
 }
