93f4a9810f11dcfc65b6be3fbd66c8083102393e
edwinjavier28 <erico.andes@gmail.com>
2024-04-16 17:26:36 -0500

    new file mayo.go

---
+++ mayo.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Definimos dos números
+	numero1 := 10
+	numero2 := 5
+
+	// Verificamos si numero1 es mayor que numero2
+	if numero1 > numero2 {
+		fmt.Printf("%d es mayor que %d\n", numero1, numero2)
+	} else {
+		fmt.Printf("%d no es mayor que %d\n", numero1, numero2)
+	}
+
+	// Verificamos si numero1 es menor que numero2
+	if numero1 < numero2 {
+		fmt.Printf("%d es menor que %d\n", numero1, numero2)
+	} else {
+		fmt.Printf("%d no es menor que %d\n", numero1, numero2)
+	}
+}
