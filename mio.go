011d85606a69fdff015eaacecc980c1d97322389
edwinjavier28 <erico.andes@gmail.com>
2024-04-19 14:46:46 -0500

    new file mio.go

---
+++ mio.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Se definen los numeros a sumar
+	numero := 8
+	numero1 := 10
+
+	//Se realiza la suma
+	numero + numero1 = numero2
+	fmt.Printf("%d la suma da", numero2)
+}
