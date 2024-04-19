6e8e93882d6bf3bac0d50c98755c71f06fa9554b
edwinjavier28 <erico.andes@gmail.com>
2024-04-19 16:18:08 -0500

    modified for.go

--- for.go
+++ for.go
@@ -2,20 +2,22 @@ package main
 
 import "fmt"
 
-var numero1 int
-var numero2 int
+var i int
 var secuencia int
 
 func main() {
 	//Definir los numeros de la secuencia
 	numero1 := 3
-	numero2 := 33
+	numero2 := 10
+
+	//Imprimimos resultados
+	fmt.Printf("la secuencia entre %d y %d da como resultado", numero1, numero2)
 
 	//Realizamos la secuencia
-	secuencia := numero1
-	for i := 0; i < numero2; i++ {
 
+	for i := numero1; i <= numero2; i++ {
+		//Mostramos el resultado
+
+		fmt.Println(i)
 	}
-	//Mostramos el resultado
-	fmt.Printf("la secuencia desde %d hasta %d da como resultado %d", numero1, numero2, secuencia)
 }
