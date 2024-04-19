2be4dd2eb446c3d0414dce54040d5ae64de11e50
edwinjavier28 <erico.andes@gmail.com>
2024-04-19 15:13:42 -0500

    modified mio.go, new file rest.go

--- mio.go
+++ mio.go
@@ -2,12 +2,15 @@ package main
 
 import "fmt"
 
+var suma int
+
 func main() {
 	//Se definen los numeros a sumar
 	numero := 8
 	numero1 := 10
 
 	//Se realiza la suma
-	numero + numero1 = numero
-	fmt.Printf("%d/n la suma da", numero)
+	suma := numero + numero1
+	//Resultado de la suma
+	fmt.Printf("%d mas %d da como resultado %d/", numero, numero1, suma)
 }
---
+++ rest.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Se definen los numeros de la resta
+	numero1 := 33
+	numero2 := 11
+
+	//Se realiza la resta
+	numero1 - numero2 := resta
+
+	//Resultado de la resta
+	fmt.Printf("%d menos %d da como resultado %d/n", numero1, numero2, resta)
+}
