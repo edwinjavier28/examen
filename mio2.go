4aec025b6aa87bd468d143df203227246c77afd3
edwinjavier28 <erico.andes@gmail.com>
2024-04-26 17:18:18 -0500

    modified mio2.go

--- mio2.go
+++ mio2.go
@@ -4,24 +4,33 @@ import "fmt"
 
 var suma int
 
-func sumarelementos(elementos []int) int {
+func sumarElementos(elementos []int) int {
 	suma := 0
 	for _, elemento := range elementos {
 		suma += elemento
 	}
 	return suma
 }
+func invertirLetras(caracteres []rune) {
+	longitud := len(caracteres)
+	for i := 0; i < longitud/2; i++ {
+		caracteres[i], caracteres[longitud-i-1] = caracteres[longitud-i-1], caracteres[i]
+	}
+}
 
 func main() {
 
 	elementos := []int{1, 2, 3, 4, 5, 7, 8, 9, 0}
-	caracteres := []string{"a, b, c, d, e, g, h, i, j"}
+	caracteres := []rune{'c', 'a', 's', 'a'}
 	slice := make([]int, len(elementos))
-	slice2 := make([]string, len(caracteres))
+	slice2 := make([]rune, len(caracteres))
 	copy(slice, elementos)
 	copy(slice2, caracteres)
-	fmt.Println("Slice1\n", elementos, "\n", "Slice2", "\n", caracteres)
+	fmt.Println(" Slice 1:\n", elementos, "\n", "Slice 2:", "\n", string(caracteres))
 
-	suma := sumarelementos(elementos)
-	fmt.Printf("La suma de los elementos del vector es: %d", suma)
+	suma := sumarElementos(elementos)
+	fmt.Printf("La suma de los elementos del Slice 1: %d", suma)
+
+	invertirLetras(caracteres)
+	fmt.Println("\nLas letras invertidas del Slice 2:", string(caracteres))
 }
