3f6051abd21e6bba47e9f0a41bd09c31c320861c
edwinjavier28 <erico.andes@gmail.com>
2024-04-27 13:37:11 -0500

    modified mio2.go

--- mio2.go
+++ mio2.go
@@ -11,26 +11,58 @@ func sumarElementos(elementos []int) int {
 	}
 	return suma
 }
-func invertirLetras(caracteres []rune) {
+func invertirLetras(caracteres []string) []string {
 	longitud := len(caracteres)
-	for i := 0; i < longitud/2; i++ {
-		caracteres[i], caracteres[longitud-i-1] = caracteres[longitud-i-1], caracteres[i]
-	}
-}
+	invertido := make([]string, longitud) // Crear un nuevo slice para almacenar el resultado invertido
 
+	for i := 0; i < longitud; i++ {
+		invertido[i] = caracteres[longitud-i-1]
+	}
+	return invertido
+}
+func reconocerPalindromo(caracteres []string) bool {
+	letras := invertirLetras(caracteres)
+	a := recorrerStrings(letras)
+	b := recorrerStrings(caracteres)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(letras, "Letras en recorrer palindromo")
+
+	if a == b {
+		return true
+	} else {
+		return false
+	}
+
+}
+func recorrerStrings(caracteres []string) string {
+	var normal string
+	for i := 0; i < len(caracteres); i++ {
+		normal += caracteres[i]
+	}
+	return normal
+
+}
 func main() {
 
 	elementos := []int{1, 2, 3, 4, 5, 7, 8, 9, 0}
-	caracteres := []rune{'c', 'a', 's', 'a'}
+	caracteres := []string{"r", "a", "t", "o", "n"}
 	slice := make([]int, len(elementos))
-	slice2 := make([]rune, len(caracteres))
+	slice2 := make([]string, len(caracteres))
 	copy(slice, elementos)
 	copy(slice2, caracteres)
-	fmt.Println(" Slice 1:\n", elementos, "\n", "Slice 2:", "\n", string(caracteres))
+	fmt.Println(" Slice 1:\n", elementos, "\n", "Slice 2:", "\n", (caracteres))
 
 	suma := sumarElementos(elementos)
 	fmt.Printf("La suma de los elementos del Slice 1: %d", suma)
 
 	invertirLetras(caracteres)
-	fmt.Println("\nLas letras invertidas del Slice 2:", string(caracteres))
+	fmt.Println("\nLas letras invertidas del Slice 2:", (caracteres))
+	c := reconocerPalindromo(caracteres)
+	if c == true {
+		fmt.Println(c, "Es palindromo")
+	} else {
+		fmt.Println(c, "No es palindromo")
+	}
+
 }
