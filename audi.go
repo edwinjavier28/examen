ae4200565d7c4bc91b1e2663703a96f9829402ab
edwinjavier28 <erico.andes@gmail.com>
2024-05-08 12:46:00 -0500

    modified audi.go

--- audi.go
+++ audi.go
@@ -5,21 +5,22 @@ import "fmt"
 func main() {
 
 	c := new(Audifonos)
-	audifono_L := c.NewAudifonos("rojo", 10, "l", "bose")
-	audifono_R := c.NewAudifonos("rojo", 10, "R", "bose")
+	audifono_L := c.NewAudifonos("rojo", 10, "L", "Bose", "F1")
+	audifono_R := c.NewAudifonos("rojo", 10, "R", "Bose", "F1")
 	Escuchar := c.EscucharAudifonos(audifono_L, audifono_R)
 	fmt.Println(Escuchar)
 }
 
 type Audifonos struct {
-	Color string
-	Peso  int
-	Lado  string
-	Marca string
+	Color  string
+	Peso   int
+	Lado   string
+	Marca  string
+	Modelo string
 }
 
-func (c *Audifonos) NewAudifonos(Color string, Peso int, Lado string, Marca string) Audifonos {
-	return Audifonos{Color: Color, Peso: Peso, Lado: Lado, Marca: Marca}
+func (c *Audifonos) NewAudifonos(Color string, Peso int, Lado string, Marca string, Modelo string) Audifonos {
+	return Audifonos{Color: Color, Peso: Peso, Lado: Lado, Marca: Marca, Modelo: Modelo}
 }
 func (c *Audifonos) EscucharAudifonos(audifono_L, audifono_R Audifonos) int {
 	fmt.Println("Escuchando audifono L", audifono_L)
